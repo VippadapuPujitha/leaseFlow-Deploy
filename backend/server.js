3271fb84644e4ec07c2b27f00f69e593ef1d60e7
Tulasi <tulasivulli2005@gmail.com>
2026-06-02 00:10:32 +0530

    Setup authentication module structure

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Auth Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("LeaseFlow API Running");
