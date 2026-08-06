f8287d2a50b05513f8133cced71816df9a6e2f47
VippadapuPujitha <vippadapupujitha12@gmail.com>
2026-08-06 21:50:28 +0530

    Update CORS for new Vercel URL

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://lease-flow-deploy-git-main-vippadapu-pujitha.vercel.app",
+      "https://lease-flow-deploy-mqrfhqnhp-vippadapu-pujitha.vercel.app",
     ],
     credentials: true,
   })
