4c6dd81a39090aec35ad30dd56d703b29eef6690
VippadapuPujitha <vippadapupujitha12@gmail.com>
2026-08-06 23:17:06 +0530

    Node mailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,15 +3,16 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, otp) => {
   try {
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: email,
